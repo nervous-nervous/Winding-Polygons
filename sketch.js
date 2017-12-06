d326349ef2ec95da11dd4eeb58e47215dcbc5535
jeffreyzyf <jeffreyzyf@163.com>
2017-12-05 21:22:05 -0500

    Update sketch.js
    fix some bug.

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ var ax;
 var ay;
 var n = 0;
 var flag = 0;
+var Wn;
 
 function setup() {
   // put setup code here
@@ -55,56 +56,77 @@ function draw() {
 	  ellipse(ax, ay, 10, 10);
 	  textAlign(CENTER);
 	  textSize(32);
-      text(WindingNumber(ax, ay, x, y, n), width/2, height/2);
+      	  if (Wn == 0.5){
+		text("Don't put the point on a line! Please choose a new point.", width/2, height/2);
+		flag = 3;
+	  }
+	  else
+	      text("The Winding Number is " + Wn, width/2, height/2);
   }
 
 
 }
 
 function mouseClicked(){
-	if(flag == 0){
-	 if ( abs(mouseX-x[0])<=5&&abs(mouseY-y[0])<=5){
-		 flag = 1;
-		 x[n] = x[0];
-		 y[n] = y[0];
-		 n++
-	 }
-	 else{
-		x[n] = mouseX;
-		y[n] = mouseY;
-		n++;
-	 }
-	}
-	else if(flag == 1){
-		flag = 2;
-		ax = mouseX;
-		ay = mouseY;
-	}
-	else{
-		flag = 0;
-		n = 0;
-
+	if (mouseX > 0 && mouseX < 1600 && mouseY > 0 && mouseY < 900){
+		if (flag == 0){
+			if ( abs(mouseX-x[0])<=5&&abs(mouseY-y[0])<=5){
+				flag = 1;
+				x[n] = x[0];
+				y[n] = y[0];
+				n++
+			}
+			else{
+				x[n] = mouseX;
+				y[n] = mouseY;
+				n++;
+			}
+		}
+		else if (flag == 1 || flag == 3){
+			flag = 2;
+			ax = mouseX;
+			ay = mouseY;
+/* 			ax = 100;
+			ay = 100;
+			x[0]=150;
+			y[0]=50;
+			x[1]=50;
+			y[1]=100;
+			x[2]=150;
+			y[2]=100;
+			x[3]=150;
+			y[3]=50;
+			n=4 */
+			Wn = WindingNumber(ax, ay, x, y, n)
+		}
+		else{
+			flag = 0;
+			n = 0;
+		
+		}
 	}
 }
 
-function isLeft(x0, y0, x1, y1, x2, y2){
+function ccw(x0, y0, x1, y1, x2, y2){
 	return ( (x1 - x0) * (y2 - y0) - (x2 - x0) * (y1 - y0) );
 }
-
 function WindingNumber(ax, ay, x, y, n){
 	var wn = 0;
 	for (var j = 0; j < n-1; j++){
-		if (y[j] <= ay) {
+		if (ccw(x[j], y[j], x[j+1], y[j+1], ax, ay) == 0 && ((x[j] >= ax && x[j+1] < ax) || (x[j] < ax && x[j+1] >= ax))){
+			wn = 0.5;
+			break;
+		}
+		if (y[j] < ay) {
 			if(y[j+1]>=ay){
-				//if (isLeft(x[j], y[j], x[j+1], y[j+1], ax, ay) > 0)
-        if (isLeft( ax, ay, x[j], y[j], x[j+1], y[j+1]) > 0)
-				wn++;
+				if (ccw(x[j], y[j], x[j+1], y[j+1], ax, ay) > 0)
+					wn++;
 			}
-
+			
 		}
 		else{
-			if (y[j+1] <= ay){
-				if (isLeft(x[j], y[j], x[j+1], y[j+1], ax, ay) < 0)
+			if (y[j+1] < ay){
+				if (ccw(x[j], y[j], x[j+1], y[j+1], ax, ay) < 0)
 					wn--;
 			}
 		}
